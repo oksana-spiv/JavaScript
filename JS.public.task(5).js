5a4297a326bb3babb4cd858506ec504c51b5d95c
oksana-spiv <116691493+oksana-spiv@users.noreply.github.com>
2023-05-04 04:51:05 +0300

    Update JS.public.task(5).js

--- JS.public.task(5).js
+++ JS.public.task(5).js
@@ -17,8 +17,29 @@ const product = {
     if (product.userRegistred) {
       console.log('Товар в корзине');
     } else {
-      console.log('Вам еобходимо зарегистрироваться');
+      console.log('Вам необходимо зарегистрироваться ->product.reg');
     }
   }
 }
 product.buyProduct();
+product.reg();
+product.buyProduct();
+
+const productTest = {
+  text['hello'], //[]помогают содержимое распознать как текст
+  price: 1000}
+//for (const key in object){
+//  console.log (key + ":" + object[key]);
+//}
+for (const key in productTest){
+  console.log (key + ":" + object[key]);
+}
+
+
+------
+//преобразоваие объекта в массив
+const object = {
+  1:'Ivanov',
+  2:'Petrov',
+};
+const students = object.map((student) => 'student: ${student}');
