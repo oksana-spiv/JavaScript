157858681650150526c794f1bbad19f39855fcd4
oksana-spiv <116691493+oksana-spiv@users.noreply.github.com>
2023-05-20 15:18:34 +0300

    Update JavaScript(7).js

--- JavaScript(7).js
+++ JavaScript(7).js
@@ -30,18 +30,38 @@
 []()
 
 // 1. Ко всем элементам, имеющим класс "dropdown-item" добавить еще один класс "super-dropdown", необходимо использовать методы forEach и querySelectorAll и свойство classList у элементов.
+ let elems = document.querySelectorAll('.dropdown-item');
+    elems.forEach(elem => elem.classList.add('super-dropdown'));
 
 
 // 2. У элемента с классом btn необходимо убрать класс "btn-secondary", если он присутствует у этого элемента, либо добавить, если такого класса у элемента не было.
+const btnScnd = document.querySelector('.btn');
+btnScnd.classList.remove('btn-secondary');
+//.classList.toggle('btn-secondary');
 
 
-// 3. Необходимо удалить класс "dropdown-menu" у элемента, у которого присутствует класс "menu".
-
+// 3. Необходимо удалить класс "dropdown-menu" у элемента, у которого присутствует класс "menu"
+const clMenu = document.querySelector('.menu');
+clMenu.classList.remove('dropdown-menu');
+  
+  
 // 4. Используя метод insertAdjacentHTML добавьте после div'a с классом "dropdown" следующую разметку:
 // `<a href="#">link</a>`
+const dropdown = document.querySelector('.dropdown');
+dropdown.insertAdjacentHTML('afterend', '<a href="#">link</a>');
+
 
 // 5. У элемента с id "dropdownMenuButton" замените id на "superDropdown".
+const repId = document.getElementById('dropdownMenuButton');
+repId.id = 'superDropdown';
 
-// 6. Добавьте атрибут data-dd со значением 3 элементу у которого существует атрибут "aria-labelledby" равный "dropdownMenuButton" используя dataset.
+
+// 6. Добавьте атрибут data-dd со значением 3 элементу, у которого существует атрибут "aria-labelledby" равный "dropdownMenuButton" используя dataset.
+document.querySelector('[aria-labelledby="dropdownMenuButton"]')
+ .dataset
+ .dd = "3";
 
 // 7. Удалите атрибут type у элемента с классом "dropdown-toggle".
+document.querySelector('.dropdown-toggle').removeAttribute('type');
+
+
