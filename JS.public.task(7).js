c596bec6c357d1bf89cd858b945e28465b23b0af
oksana-spiv <116691493+oksana-spiv@users.noreply.github.com>
2023-05-20 14:27:23 +0300

    Update JS.public.task(7).js

--- JS.public.task(7).js
+++ JS.public.task(7).js
@@ -1,3 +1,9 @@
+// 1. <div class = "block">;
+//     </div>
+// Дан блок, внутри него необходимо создать элемент div с классом item, разместить текст "элемент внутри" и задать стили:
+// Цвет текста синий, рамка сплошная черная, цвет фона #f8f8f8, внутренний отступ 16px. 
+// Добавить к данному блоку класс "item_1" (использовать setAttribute).
+
 const block = document.querySelector('.block');
 
 const itemDiv = document.createElement('div');
@@ -14,6 +20,23 @@ block.appendChild(itemDiv);
 block.setAttribute('class', 'item_1');
 
 
+
+
+
+// 2. Дан код
+// <div cass = "elem">
+// <img src="photo.png" alt="photo">
+// <div class="content">
+// <h2 class="heading">Lorem, ipsum dolor. </h2>
+// <p class="text">Lorem, ipsum dolor sit amet consectetur adipising elit. Recusandae, ea! </p>
+// </div>
+// </div>
+//2.1 Необходимо с помощью querySelector найти параграф с классом text
+// 2.2. Вывести в консоль соседний элемент h2
+// 2.3. Вывести в консоль родительский элемент content
+// 2.4. Вывести в консоль картинку
+// 2.5. Вывести в консоль родительский элемент elem 
+
 const textElem = document.querySelector('.text');
 
 const headingElem = textElem.previousElementSibling;
@@ -28,17 +51,29 @@ console.log(imgElem);
 const elemElem = contentElem.parentNode;
 console.log(elemElem);
 
+
+
+
+// 3. С помощью querySelector найти элемент h2 и вывести в консоль всех его родителей
+<div class = "item">
+    <div class = "elem">
+        <div class = "info">
+            <h2 class = "subtitle"> Lorem, ipsum dolor.</h2>
+        </div>
+    </div>
+</div>
+
 const h2Element = document.querySelector('h2.subtitle');
 
 let parentElement = h2Element.parentNode;
 console.log(parentElement);
 
-while (parentElement) {
+while (parentElement) { //пока есть родительский элемент, цикл выполняется 
     console.log(parentElement);
-    parentElement = parentElement.parentNode;
-    
+    parentElement = parentElement.parentNode; //цикл остановится на body, когда закончатся родительские элементы
 }
 
+// 4.
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const btn = document.querySelector('.btn');
