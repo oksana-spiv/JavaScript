d1cf1ee99baa647d7659d75da2ed88a880d6cfa9
oksana-spiv <116691493+oksana-spiv@users.noreply.github.com>
2023-05-26 03:48:25 +0300

    Update JS.public.task(9).js

--- JS.public.task(9).js
+++ JS.public.task(9).js
@@ -5,8 +5,19 @@
 1.3.При клике на кнопку отправить нужно проверять выбран ли активным элемент checkbox
 1.4. Если элемент не выбран, добавить текст под чекбоксом “Необходимо согласиться с условиями”
 */
+<input type="checkbox" id="agree"> Согласен с условиями
+<button id="submit">Отправить</buttton>
 
+const checkbox = document.getElementById('agree');
+const submitButton = document.getElementById('submit');
 
+submitButton.addEventListener('click', function () {
+  if (!checkbox.checked) {
+    const error = document.createElement('div'); //если не установлен флажок, то создаем элемент div...
+    error.textContent = 'Необходимо согласиться с условием';
+    checkbox.parentNode.appendChild(error)
+  }
+});
 
 /*
 2.1. В html создать 2 элемента радио кнопки (input type=”radio”) и текст “Чай”, “Кофе” соответственно
@@ -16,7 +27,21 @@
 2.4.Если выбран кофе, необходимо вывести соообщение “кофе
 закончился”
 */
+<input type="radio" name="drink" value="tea"> Чай
+<input type="radio" name="drink" value="coffee"> Кофе
+<button id="submit">Отправить</buttton>
 
+const teaButton = document.querySelector('input[name="drink"][value="tea"]');
+const coffeeButton = document.querySelector('input[name="drink"][value="coffee"]');
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', function () {
+  if (teaButton.checked) {
+    const error = document.createElement('div'); //если не установлен флажок, то создаем элемент div...
+    error.textContent = 'Необходимо согласиться с условием';
+    checkbox.parentNode.appendChild(error)
+  }
+});
 
 /*
 3.1. Создать поле ввода (пароль)
